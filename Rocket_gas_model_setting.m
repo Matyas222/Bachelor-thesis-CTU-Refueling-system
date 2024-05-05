81186862c28d92dfd4e734898a357c91d288ce89
Matyáš Meisner <146086905+Matyas222@users.noreply.github.com>
2024-05-05 22:55:36 +0200

    Add files via upload

---
+++ Rocket_gas_model_setting.m
@@ -0,0 +1,47 @@
+%GSE Pipe's params
+
+GSE_Pipe_Hydr_Diameter = 2.1; %mm
+GSE_Pipe_Cross_sectional_Area = 7.48; % 38,48 mm^2
+
+%Temperature params
+
+Outside_temperature = 20; %Celsia
+
+%N2 Rocket tank params
+
+N2_Rocket_Tank_Pressure = 1; % bar
+N2_Rocket_Tank_Volume = 0.003; %0.01 m^3
+N2_Rocket_Tank_Wall_Width = 0.01; %m
+N2_Rocket_Tank_Mass = 2.141; %kg
+N2_Rocket_Tank_Diameter = 0.146; %m
+N2_Rocket_Tank_Dens = 2000;       %assuming it is made of carbon fiber  kg/m^3
+N2_Rocket_Tank_C = 750;       %assuming it is made of carbon fiber  J/(kg*K)
+N2_Rocket_Tank_Height = 1.515; % m
+
+%N2 Gas tank params
+
+N2_Gas_Tank_Pressure = 300; % bar
+N2_Gas_Tank_Volume = 0.05; %m^3
+N2_Gas_Tank_Wall_Width = 0.01; %m
+N2_Gas_Tank_Mass = 64; %kg
+N2_Gas_Tank_Diameter = 0.229; %m
+N2_Gas_Tank_Height = 0.5; %m
+N2_Gas_Tank_Dens = 7850;   %assuming it is made of steel  kg/m^3
+N2_Gas_Tank_C = 450;      % J/(kg*K)
+
+%Hose length
+
+Hose_length = 2; %m
+
+%Rocket Pipe's params
+
+Rocket_Pipe_Hydr_Diameter = 4; %mm
+Rocket_Pipe_Cross_sectional_Area = 2; %12.57 mm^2
+
+%N20 Rocket tank params
+
+N2O_Rocket_Tank_Volume = 0.007; %m^3
+N2O_Rocket_Tank_Mass = 1.5; %kg
+N2O_Rocket_Tank_C = 853;  % J/(kg*K)
+
+
